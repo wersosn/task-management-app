04b78c92d70ff46d51bbc3a937b67281690813c5
Inga Wojtulewicz <inga_2003@op.pl>
2024-12-10 20:59:23 +0100

    MVC segregacja v2

--- TaskManager/Model/Tag.cs
+++ TaskManager/Model/Tag.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TaskManager.Model
 {
-	internal class Tag
+	public class Tag
 	{
 	}
 }
--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TaskManager.Controller;
 
 namespace TaskManager
 {
