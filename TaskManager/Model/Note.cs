213a265507d275bfddc76904bc94daffec796f38
wersosn <wersosnwsk@gmail.com>
2024-12-15 19:11:25 +0100

    Dodałam buildery i command

--- TaskManager/Model/Note.cs
+++ TaskManager/Model/Note.cs
@@ -7,19 +7,29 @@ using System.Threading.Tasks;
 
 namespace TaskManager.Model
 {
-
+	// Model Notatki
 	public class Note
 	{
+        // Atrybuty:
+        public string Id { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
-		// public Tag Tag { get; set; }
-		//public Category Category { get; set; }
+        public ITag Tag { get; set; }
+        public ICategory Category { get; set; }
 
-		// do poprawienia po dodaniu Tag i Category
-		public Note(string title, string description)
+        public Note() { }
+        public Note(string title, string description, ITag tag, ICategory category)
 		{
 			Title = title;
 			Description = description;
+			Tag = tag;
+			Category = category;
 		}
-	}
+
+        public override string ToString()
+        {
+            return $"Notatka: {Title}\nOpis: {Description}\nTag: {Tag?.Name}\nKategoria: {Category?.CategoryName}";
+        }
+    }
 }
+
--- TaskManager/Model/Task.cs
+++ TaskManager/Model/Task.cs
@@ -7,22 +7,53 @@ using System.Threading.Tasks;
 
 namespace TaskManager.Model
 {
-	public class Task
-	{
-		public string Title { get; set; }
-		public string Description { get; set; }
-		// public Tag Tag { get; set; }
-		//public Category Category { get; set; }
-		public DateTime DueDate { get; set; }
-		public string Priority { get; set; }
+    // Model Zadania
+    public class Task
+    {
+        // Atrybuty:
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ITag Tag { get; set; }
+        public ICategory Category { get; set; }
+        public DateTime Deadline { get; private set; }
+        public string Priority { get; private set; }
+        public bool IsDone { get; private set; }
 
-		// do poprawienia po dodaniu Tag i Category
-		public Task(string title, string description, DateTime dueDate, string priority)
-		{
-			Title = title;
-			Description = description;
-			DueDate = dueDate;
-			Priority = priority;
-		}
-	}
+        public Task() { }
+
+        public Task(string title, string description, ITag tag, ICategory category, DateTime deadline, string priority, bool isDone)
+        {
+            Title = title;
+            Description = description;
+            Tag = tag;
+            Category = category;
+            Deadline = deadline;
+            Priority = priority;
+            IsDone = false;
+        }
+
+
+        // Metody do ustawiania terminów, priorytetów i oznaczania jako wykonane
+        public void UstalTermin(DateTime deadline)
+        {
+            Deadline = deadline;
+        }
+
+        public void UstawPriorytet(string priority)
+        {
+            Priority = priority;
+        }
+
+        public void OznaczJakoWykonane()
+        {
+            IsDone = true;
+        }
+
+        public override string ToString()
+        {
+            return $"Zadanie: {Title}\nOpis: {Description}\nTag: {Tag?.Name}\nKategoria: {Category?.CategoryName}\nTermin: {Deadline}\nPriorytet: {Priority}\nWykonane: {IsDone}";
+        }
+    }
 }
+
