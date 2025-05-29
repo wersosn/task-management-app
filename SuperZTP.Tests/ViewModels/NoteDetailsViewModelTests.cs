17f49ec06ccec8bb312730fd9eec6a649c2f430c
Bartłomiej Karanowski <aztek092016@gmail.com>
2025-05-29 17:39:14 +0200

    testy

---
+++ SuperZTP.Tests/ViewModels/NoteDetailsViewModelTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using SuperZTP.ViewModels;
+using SuperZTP.Model;
+using SuperZTP.Stores;
+using System;
+
+namespace SuperZTP.Tests.ViewModels
+{
+    public class NoteDetailsViewModelTests
+    {
+        [Fact]
+        public void Properties_ShouldReflectSelectedNote()
+        {
+            var note = new Note
+            {
+                Title = "Test",
+                Description = "Opis",
+                Category = new Category("Kat"),
+                Tag = new Tag("Tag")
+            };
+            var store = new SelectedTaskStore { SelectedNote = note };
+            var vm = new NoteDetailsViewModel(store);
+
+            Assert.True(vm.HasSelectedNote);
+            Assert.Equal("Test", vm.Title);
+            Assert.Equal("Opis", vm.Description);
+            Assert.Equal("Kat", vm.Category);
+            Assert.Equal("Tag", vm.Tag);
+        }
+
+        [Fact]
+        public void Properties_ShouldBeNull_WhenNoteIsNull()
+        {
+            var store = new SelectedTaskStore { SelectedNote = null };
+            var vm = new NoteDetailsViewModel(store);
+
+            Assert.False(vm.HasSelectedNote);
+            Assert.Null(vm.Title);
+            Assert.Null(vm.Description);
+            Assert.Null(vm.Category);
+            Assert.Null(vm.Tag);
+        }
+    }
+}
---
+++ SuperZTP.Tests/ViewModels/TaskDetailsViewModelTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using SuperZTP.ViewModels;
+using SuperZTP.Model;
+using SuperZTP.Stores;
+using System;
+
+namespace SuperZTP.Tests.ViewModels
+{
+    public class TaskDetailsViewModelTests
+    {
+        [Fact]
+        public void Properties_ShouldReflectSelectedTask()
+        {
+            var task = new SuperZTP.Model.Task
+            {
+                Title = "Zadanie",
+                Description = "Opis",
+                Deadline = new DateTime(2024, 1, 1),
+                Category = new Category("Kat"),
+                Tag = new Tag("Tag"),
+                Priority = "Wysoki",
+                IsDone = true
+            };
+            var store = new SelectedTaskStore { SelectedTask = task };
+            var vm = new TaskDetailsViewModel(store);
+
+            Assert.True(vm.HasSelectedTask);
+            Assert.Equal("Zadanie", vm.Title);
+            Assert.Equal("Opis", vm.Description);
+            Assert.Equal("Kat", vm.Category);
+            Assert.Equal("Tag", vm.Tag);
+            Assert.Equal("Wysoki", vm.Priority);
+            Assert.Equal("Ukończone", vm.IsDone);
+            Assert.Equal(task.Deadline, vm.Date);
+        }
+
+        [Fact]
+        public void HasNoSelection_ShouldBeTrue_WhenNoNoteOrTask()
+        {
+            var store = new SelectedTaskStore();
+            var vm = new TaskDetailsViewModel(store);
+            Assert.True(vm.HasNoSelection);
+        }
+    }
+}
